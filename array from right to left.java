35cb29f2117bb123176816f20d5f0191e8bd2888
UDHAYAPRAKASH S <udhayaprakashnvpt@gmail.com>
2025-08-06 20:42:02 +0530

    array from right to left.java

---
+++ array from right to left.java
@@ -0,0 +1,21 @@
+//accept the elements into an array and display the array from right to left
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        for(int i=n-1;i>=0;i--)
+            System.out.print(a[i]+" ");
+    }
+}
