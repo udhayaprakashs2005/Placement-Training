0c224c939efcc4faa8a53787e22fa15ec53a5f1b
UDHAYAPRAKASH S <udhayaprakashnvpt@gmail.com>
2025-08-08 20:31:34 +0530

    smallest element in the array.java

---
+++ smallest element in the array.java
@@ -0,0 +1,30 @@
+//accept a array elements from the user and display the smallest element in the array
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int small=a[0];
+        for(int i=1;i<n;i++){
+            
+        
+        if(a[i]<small){
+            small=a[i];
+            
+        }
+        }
+        System.out.println(small);
+    }
+}
