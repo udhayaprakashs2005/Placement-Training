ea95b19ba05e8bc3873e366f169f3d4045b71a16
UDHAYAPRAKASH S <udhayaprakashnvpt@gmail.com>
2025-08-12 20:11:43 +0530

    length of the string.java

---
+++ length of the string.java
@@ -0,0 +1,18 @@
+//find the length of the string without using in-built function
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+            System.out.println(s.length());
+        }
+}
